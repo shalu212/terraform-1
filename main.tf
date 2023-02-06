1821ed884d619f7e6c3672ce819a54d39b6a07a9
Shalini <111748599+shalu212@users.noreply.github.com>
2023-02-06 17:15:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,6 @@
 
 provider "aws"{
 region = "ap-south-1" 
-access_key = "AKIATZU67RERRYOOPH2W"
-secret_key = "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"
+access_key = "AKIA3UJ2NXQFASH3XOAB"
+secret_key = "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"
 }
